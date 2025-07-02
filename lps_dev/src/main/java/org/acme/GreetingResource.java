de7325dce79494d124af83f14f9784ff92376360
nagatani_LPS <nagatani@link-ps.co.jp>
2025-07-03 00:06:22 +0900

    DBからのデータ取得

--- lps_dev/src/main/java/org/acme/GreetingResource.java
+++ lps_dev/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,28 @@
 package org.acme;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
 
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    DSLContext dsl;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        Result<org.jooq.Record> result = dsl.fetch("SELECT id, name FROM users");
+        StringBuilder sb = new StringBuilder();
+        for (Record r : result) {
+            sb.append(r.get("id")).append(": ").append(r.get("name")).append("\n");
+        }
+        return sb.toString();
     }
 }
